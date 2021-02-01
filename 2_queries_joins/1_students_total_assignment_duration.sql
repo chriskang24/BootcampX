e376940dfcbf38d043bc11e26a40dd323e15d8bb
Christopher Kang <christopherjkang24@gmail.com>
2021-02-01 17:52:42 -0500

    add complete join exercises

---
+++ 2_queries_joins/1_students_total_assignment_duration.sql
@@ -0,0 +1,9 @@
+-- Get the total amount of time that a student has spent on all assignments.
+-- This should work for any student but use 'Ibrahim Schimmel' for now.
+-- Select only the total amount of time as a single column.
+
+SELECT SUM(assignment_submissions.duration) AS "total_duration"
+FROM students
+JOIN assignment_submissions
+ON students.id = assignment_submissions.student_id
+WHERE students.name = 'Ibrahim Schimmel';
